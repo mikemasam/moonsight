95a0020f08a6721fcee1321f6f34fcb7fcb54661
sudo <meshackmasam@gmail.com>
2021-08-02 00:23:54 +0800

    routes logs added

---
+++ sample/index.js
@@ -0,0 +1,4 @@
+export default async (ctx) => {
+  return async () => {
+  }
+}
--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 import kernel from './index.js';
+import path from 'path';
 kernel({
-  port: 3002
+  port: 3003,
+  apiPath: path.resolve('sample')
 }).catch(console.log);
