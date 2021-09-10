46f1b1275c824845ae2ba9a94262ce452d266e37
sudo <meshackmasam@gmail.com>
2021-09-10 20:15:45 +0800

    development

--- responders/Request.js
+++ responders/Request.js
@@ -4,22 +4,22 @@ class RequestState{
   }
 
   get(name){
-    if(req.__type == 'ihttp'){
+    if(this.req.__type == 'ihttp'){
       return this.req.locals[name];
-    }else if(req.__type == 'isocket'){
+    }else if(this.req.__type == 'isocket'){
       return this.req.socket.locals[name];
-    }else if(req.__type == 'icore'){
+    }else if(this.req.__type == 'icore'){
       return this.req.socket.locals[name];
     }
     return null;
   }
 
   put(name, data){
-    if(req.__type == 'ihttp'){
+    if(this.req.__type == 'ihttp'){
       this.req.locals[name] = data;
-    }else if(req.__type == 'isocket'){
+    }else if(this.req.__type == 'isocket'){
       this.req.socket.locals[name] = data;
-    }else if(req.__type == 'icore'){
+    }else if(this.req.__type == 'icore'){
       this.req.socket.locals[name] = data;
     }
   }
