51ad9ba2b97124fc3ab9b3d561f15a0801bef8f1
sudo <meshackmasam@gmail.com>
2022-02-13 04:27:47 +0800

    success response shorthand added

--- responders/Response.js
+++ responders/Response.js
@@ -1,4 +1,10 @@
 import jsonWriter from './json-writer.js';
-export default (data, { message = 'success', status = 200 } = {}) => {
+export default (data, opts) => {
+  let status = Number(opts) !== NaN ? opts : 200;
+  let message = "";
+  if(typeof opts == "object"){
+    message = opts?.message ? opts?.message : "";
+    status = Number(opts?.status) !== NaN ? opts?.status : status;
+  }
   return jsonWriter({ data, status, message }, {}, data);
 }
