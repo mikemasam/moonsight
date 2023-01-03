b8b2c59be2413d2845599d47ab4c01298df333a6
sudo <meshackmasam@gmail.com>
2023-01-03 10:48:21 +0800

    development

--- doctor/index.js
+++ doctor/index.js
@@ -2,6 +2,9 @@ import HttpDoctor from './http.js';
 import SocketDoctor, 
 { initSocket, cleanSocket } from './socket.js';
 export default async function doctor(opts){
+  if(process.env.APP_ENV != 'development'){
+    throw `Expected APP_ENV == development, Found APP_ENV == ${process.env.APP_ENV}`;
+  }
   if(opts.socket) await initSocket(opts);
   return {
     opts,
