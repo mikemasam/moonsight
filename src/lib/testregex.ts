b4a7b596341d0a9cc272acadecb819cde22e8769
sudo <meshackmasam@gmail.com>
2023-11-05 15:30:26 +0300

    fix regex validation

--- src/lib/testregex.ts
+++ src/lib/testregex.ts
@@ -1,9 +1,7 @@
 const TestRegex = {
   testemail: (email: string): boolean => {
     if (!email) return false;
-    return /(^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])$)?/.test(
-      email
-    );
+    return /^[\w.-0-9]+@[\w.-]+\.\w{2,10}$/.test(email);
   },
 
   testphone: (phone: string): boolean => {
@@ -13,12 +11,17 @@ const TestRegex = {
   testname: (text: string): boolean => {
     if (!text) return false;
     return /(^(\w|\d|\s|\_|\.|\*|\\|\/|\[|\]|\>|\<|\=|\"|\)|\(|\{|\}|\,|\-){1,100}$)/.test(
-      text
+      text,
     );
   },
   testdesc: (text: string): boolean => {
     if (!text) return false;
     return /(^[\s\S]{1,1000}$)/.test(text);
+  },
+  cleanphone: (bare_phone: string) => {
+    return `${
+      bare_phone.indexOf("0") == 0 ? bare_phone.substring(1) : bare_phone
+    }`;
   },
 };
 
