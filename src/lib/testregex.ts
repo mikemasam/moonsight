e4964dfd4d94d4ee3db6f29a8ba16e3d82fb69b4
sudo <meshackmasam@gmail.com>
2023-11-05 15:35:32 +0300

    phone number cleanup added regex

--- src/lib/testregex.ts
+++ src/lib/testregex.ts
@@ -6,7 +6,7 @@ const TestRegex = {
 
   testphone: (phone: string): boolean => {
     if (!phone) return false;
-    return /^([0-9\s]{12})$/.test(phone);
+    return /^([0-9\s]{12,13})$/.test(phone);
   },
   testname: (text: string): boolean => {
     if (!text) return false;
@@ -19,9 +19,10 @@ const TestRegex = {
     return /(^[\s\S]{1,1000}$)/.test(text);
   },
   cleanphone: (bare_phone: string) => {
-    return `${
-      bare_phone.indexOf("0") == 0 ? bare_phone.substring(1) : bare_phone
-    }`;
+    return `${(bare_phone.indexOf("0") == 0
+      ? bare_phone.substring(1)
+      : bare_phone
+    ).replace(/[\s+-]/, "")}`;
   },
 };
 
